681e7e44a894d55fbacb08ab5898b1e39139c460
Gleicon Moraes <gleicon@gmail.com>
2022-08-18 17:44:42 -0300

    metrics

---
+++ src/stats.rs
@@ -0,0 +1,14 @@
+/*
+Search Stats per index
+Top X queries sorted by frequency with more results
+Top X queries sorted by frequency with zero results
+Top X documents/results clicked (?)
+
+General search metrics
+Most searched index
+Documents per index
+HTTP Errors
+
+*/
+
+struct SearchStats {}
